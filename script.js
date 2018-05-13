9292a274c2fca1628b3ef5102921b41c7217178c
SylvainJunca <sylvain.junca@gmail.com>
2018-05-13 13:17:30 -0400

    Integrate promise function

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(document).ready();
 
 var channels = ["Freecodecamp", "Ninja", "GEICOGaming", "pokemon", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-
+const getJsonWithPromise = function(url){
+  return new Promise(function(resolve, reject){
+    $.getJSON(url, function(jsonResult){
+      resolve(jsonResult)
+    })
+  })
+}
 
 
 for (var i = 0; i < channels.length; i++){
-var promise = $.getJSON('https://wind-bow.glitch.me/twitch-api/users/' + channels[i]);
-promise.then(function(twitchUser){
+getJsonWithPromise('https://wind-bow.glitch.me/twitch-api/users/' + channels[i]).then(function(twitchUser){
   console.log(twitchUser);
   document.getElementById("display").innerHTML += " <img src ='" + twitchUser.logo + "' width='80px'> " + twitchUser.display_name;
 });
-var promise2 = $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/' + channels[i]);
-promise2.then(function(twitchStream){
+getJsonWithPromise('https://wind-bow.glitch.me/twitch-api/streams/' + channels[i]).then(function(twitchStream){
   if(twitchStream.stream == null) {
     document.getElementById("display").innerHTML += " is Offline<br><hr>"
   }
